3299c6d07876f786080f33281aae8f884329d474
Hyeseon <hsvogo@gmail.com>
2022-04-29 19:08:57 +0900

    stack

--- programmers/stack1.js
+++ programmers/stack1.js
@@ -5,30 +5,10 @@ console.log(solution(progresses, speeds));
 
 function solution(progresses, speeds) {
   let ans = [];
-  let dev = [];
-  for (let i = 0; i < progresses.length; i++) {
-    let days = 0;
-    while (progresses[i] < 100) {
-      progresses[i] += speeds[i];
-      days++;
-    }
-    dev.push(days);
-  }
-
-  let standard = dev.shift();
-  let count = 1;
-
-  while (dev.length !== 0) {
-    let num = dev.shift();
-    if (standard >= num) {
-      count++;
-    } else {
-      ans.push(count);
-      standard = num;
-      count = 1;
-    }
-  }
-  ans.push(count);
-
+  let days = progresses.map((progresses, idx) =>
+    Math.ceil((100 - progresses) / speeds[idx])
+  );
+  let maxDay = days[0];
+  console.log(maxDay);
   return ans;
 }
